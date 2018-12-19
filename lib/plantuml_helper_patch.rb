ea5a38d7a3160ab8caaa2810c359cca458f09b43
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2018-12-19 10:39:22 +0100

    replaced alias_method_chain with prepend

--- lib/plantuml_helper_patch.rb
+++ lib/plantuml_helper_patch.rb
@@ -2,11 +2,10 @@ require_dependency 'redmine/wiki_formatting/textile/helper'
 
 module PlantumlHelperPatch
   def self.included(base) # :nodoc:
-    base.send(:include, HelperMethodsWikiExtensions)
+    base.send(:prepend, HelperMethodsWikiExtensions)
 
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
-      alias_method_chain :heads_for_wiki_formatter, :plantuml
     end
   end
 end
@@ -14,8 +13,9 @@ end
 module HelperMethodsWikiExtensions
   # extend the editor Toolbar for adding a plantuml button
   # overwrite this helper method to have full control about the load order
-  def heads_for_wiki_formatter_with_plantuml
+  def heads_for_wiki_formatter
     return if @heads_for_wiki_plantuml_included
+    super
     content_for :header_tags do
       javascript_include_tag('jstoolbar/jstoolbar-textile.min') +
         javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language.to_s.downcase}") +
