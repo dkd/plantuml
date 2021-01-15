f9cdf996fc834d24d734b1e8fd38d7339d9ac44c
Sebastian Gumprich <rndmh3ro@users.noreply.github.com>
2021-01-15 12:48:47 +0100

    fix console log error

--- lib/plantuml/hooks/views_layouts_hook.rb
+++ lib/plantuml/hooks/views_layouts_hook.rb
@@ -4,10 +4,6 @@ module Plantuml
       def view_layouts_base_html_head(_context)
         stylesheet_link_tag(:plantuml, plugin: 'plantuml')
       end
-
-      def view_layouts_base_body_bottom(_context)
-        javascript_include_tag(:plantuml, plugin: 'plantuml')
-      end
     end
   end
 end
