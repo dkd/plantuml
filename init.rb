4af8c6a93f46853bb6b45168d3eefd92e6b483e6
Sebastian Gumprich <rndmh3ro@users.noreply.github.com>
2021-01-15 12:48:47 +0100

    update compatible redmine version to 4.1 (#37)

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :plantuml do
   version '0.5.1'
   url 'https://github.com/dkd/plantuml'
 
-  requires_redmine version: '2.6'..'4.0'
+  requires_redmine version: '2.6'..'4.1'
 
   settings(partial: 'settings/plantuml',
            default: { 'plantuml_binary' => {}, 'cache_seconds' => '0', 'allow_includes' => false })
