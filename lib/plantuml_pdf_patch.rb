a9772936d068841975b991fd8ceff97c05176ea6
krutin <krutin@krista.ru>
2022-06-22 17:18:57 +0300

    fix pdf helper for Rails@5

--- lib/plantuml_pdf_patch.rb
+++ lib/plantuml_pdf_patch.rb
@@ -4,20 +4,19 @@ module PlantumlPdfPatch
   require_dependency 'redmine/export/pdf'
 
   def self.included(base) # :nodoc:
-    base.send(:include, InstanceMethod)
+    base.send(:prepend, InstanceMethod)
 
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
-      alias_method_chain :get_image_filename, :plantUml
     end
   end
 
   module InstanceMethod
-    def get_image_filename_with_plantUml(attrname)
+    def get_image_filename(attrname)
       if attrname =~ %r{^/plantuml/png/(\w+)\.png$}i
         PlantumlHelper.plantuml_file(Regexp.last_match[1], '.png')
       else
-        get_image_filename_without_plantUml(attrname)
+        super(attrname)
       end
     end
   end
